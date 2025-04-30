da8cc91092b195472e980fa538f796b7880336c0
Tan Cao <tancaodev.github@gmail.com>
2025-04-30 19:56:20 +0700

    feat: Navbar, Hero Section

---
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "./HeroSection"
+
+const LandingPage = () => {
+  return (
+    <div>
+        <HeroSection />
+        
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import Navbar from '@/components/Navbar'
+
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='h-full w-full'>
+            <Navbar />
+            <main
+                className={`h-full flex w-full flex-col`}
+                style={{
+                    paddingTop: `${NAVBAR_HEIGHT}px`
+                }}
+            >
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default layout
