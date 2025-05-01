864bb941a0fd9c9f3f5e8c0be3439bb71382b2ec
Tan Cao <tancaodev.github@gmail.com>
2025-05-02 05:47:27 +0700

    feat: Setup Backend feat: Setup Prisma, PostgresSQL

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+/* ROUTE IMPORT*/
+
+/* CONFIGURATION */
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+/* ROUTES */
+app.get('/', (req, res) => {
+    res.send('This is home route')
+})
+
+/* SERVER */
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
