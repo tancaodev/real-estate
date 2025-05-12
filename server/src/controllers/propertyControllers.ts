cd9974fa284493ca5b277aab7c66933893375dd7
Tan Cao <tancaodev.github@gmail.com>
2025-05-12 23:25:53 +0700

    feat: Add function to get property information and create new property with geo coordinates

--- server/src/controllers/propertyControllers.ts
+++ server/src/controllers/propertyControllers.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
 import { Prisma, PrismaClient } from '@prisma/client'
+import { wktToGeoJSON } from '@terraformer/wkt'
+import { S3Client } from '@aws-sdk/client-s3'
+import { Location } from '@prisma/client'
+import { Upload } from '@aws-sdk/lib-storage'
+import axios from 'axios'
 
 const prisma = new PrismaClient()
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION
+})
 
 export const getProperties = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -79,7 +88,145 @@ export const getProperties = async (req: Request, res: Response): Promise<void> 
                 )`
             )
         }
+
+        const completeQuery = Prisma.sql`
+            SELECT
+                p.*,
+                json_build_object(
+                    'id', l.id,
+                    'address', l.address,
+                    'city', l.city,
+                    'state', l.state,
+                    'country', l.country,
+                    'postalCode', l."postalCode",
+                    'coordinates', json_build_object(
+                        'longitude', ST_X(l."coordinates"::geometry),
+                        'latitude', ST_Y(l."coordinates"::geometry)
+                    )
+                ) as location
+            FROM
+                "Property" p
+            JOIN
+                "Location" l ON p."locationId" = l.id
+            ${whereConditions.length > 0 ? Prisma.sql`WHERE ${Prisma.join(whereConditions, ' AND ')}` : Prisma.empty}
+        `
+
+        const properties = await prisma.$queryRaw(completeQuery)
+
+        res.json(properties)
     } catch (error: any) {
-        res.status(500).json({ message: `Error retrieving manager: ${error.message}` })
+        res.status(500).json({ message: `Error retrieving properties: ${error.message}` })
+    }
+}
+
+export const getProperty = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params
+        const property = await prisma.property.findUnique({
+            where: { id: Number(id) },
+            include: {
+                location: true
+            }
+        })
+
+        if (property) {
+            const coordinates: { coordinates: string }[] =
+                await prisma.$queryRaw`SELECT ST_asText(coordinates) as coordinates FROM "Location" WHERE id = ${property.location.id}`
+
+            const geoJSON: any = wktToGeoJSON(coordinates[0]?.coordinates || '')
+            const longitude = geoJSON.coordinates[0]
+            const latitude = geoJSON.coordinates[1]
+
+            const propertyWithCoordinates = {
+                ...property,
+                location: {
+                    ...property.location,
+                    coordinates: {
+                        longitude,
+                        latitude
+                    }
+                }
+            }
+            res.json(propertyWithCoordinates)
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: `Error retrieving property: ${error.message}` })
+    }
+}
+
+export const createProperty = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const files = req.files as Express.Multer.File[]
+        const { address, city, state, country, postalCode, managerCognitoId, ...propertyData } = req.body
+
+        const photoUrls = await Promise.all(
+            files.map(async (file) => {
+                const uploadParams = {
+                    Bucket: process.env.S3_POCKET_NAME!,
+                    Key: `properties/${Date.now()}-${file.originalname}`,
+                    Body: file.buffer,
+                    ContentType: file.mimetype
+                }
+
+                const uploadResult = await new Upload({
+                    client: s3Client,
+                    params: uploadParams
+                }).done()
+
+                return uploadResult.Location
+            })
+        )
+
+        const geocodingUrl = `https://nominatim.openstreetmap.org/search?${new URLSearchParams({
+            street: address,
+            city,
+            country,
+            postalcode: postalCode,
+            format: 'json',
+            limit: '1'
+        }).toString()}`
+        const geocodingResponse = await axios.get(geocodingUrl, {
+            headers: {
+                'User-Agent': 'RealEstateApp (realestateapp@example.com)'
+            }
+        })
+        const [longitude, latitude] =
+            geocodingResponse.data[0]?.lon && geocodingResponse.data[0].lat
+                ? [parseFloat(geocodingResponse.data[0]?.lon), parseFloat(geocodingResponse.data[0]?.lat)]
+                : [0, 0]
+
+        // create location
+        const [location] = await prisma.$queryRaw<Location[]>`
+            INSERT INTO "Location" (address, city, state, country, "postalCode", coordinates)
+            VALUES (${address}, ${city}, ${state}, ${country}, ${postalCode}, ST_SetSRID(ST_MakePoint(${longitude}, ${latitude}), 4326))
+            RETURNING id, address, city, state, country, "postalCode", ST_AsText(coordinates) as coordinates;
+        `
+
+        const newProperty = await prisma.property.create({
+            data: {
+                ...propertyData,
+                photoUrls,
+                locationId: location.id,
+                managerCognitoId,
+                amenities: typeof propertyData.amenities === 'string' ? propertyData.amenities.split(',') : [],
+                highlights: typeof propertyData.highlights === 'string' ? propertyData.highlights.split(',') : [],
+                isPetsAllowed: propertyData.isPetsAllowed === 'true',
+                isParkingIncluded: propertyData.isParkingIncluded === 'true',
+                pricePerMonth: parseFloat(propertyData.pricePerMonth),
+                securityDeposit: parseFloat(propertyData.securityDeposit),
+                applicationFee: parseFloat(propertyData.applicationFee),
+                beds: parseInt(propertyData.beds),
+                baths: parseFloat(propertyData.baths),
+                squareFeet: parseInt(propertyData.squareFeet)
+            },
+            include: {
+                location: true,
+                manager: true
+            }
+        })
+
+        res.status(201).json(newProperty)
+    } catch (error: any) {
+        res.status(500).json({ message: `Error creating property: ${error.message}` })
     }
 }
--- server/src/routes/propertyRoutes.ts
+++ server/src/routes/propertyRoutes.ts
@@ -1,15 +1,15 @@
 import express from 'express'
-import { getProperties, getProperty, createProperty } from '../controllers/propertyControllers'
+import { createProperty, getProperties, getProperty } from '../controllers/propertyControllers'
 import multer from 'multer'
 import { authMiddleware } from '../middleware/authMiddleware'
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = express.Router()
 
 router.get('/', getProperties)
 router.put('/:id', getProperty)
-router.post('/', authMiddleware(['manager']), upload.array('photos') , createProperty)
+router.post('/', authMiddleware(['manager']), upload.array('photos'), createProperty)
 
 export default router
