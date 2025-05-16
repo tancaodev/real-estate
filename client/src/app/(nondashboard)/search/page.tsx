a3a248aaa18194c45579d9ed34257fda76cc2dc1
Tan Cao <tancaodev.github@gmail.com>
2025-05-16 16:36:34 +0700

    feat: Add filter that bootstraps logic from Redux search and update parameters

--- client/src/app/(nondashboard)/search/page.tsx
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -1,14 +1,37 @@
 'use client'
 
-import { NAVBAR_HEIGHT } from '@/lib/constants'
-import { useAppDispatch, useAppSelector } from '@/state/redux'
+import { useEffect } from 'react'
+import { setFilters } from '@/state'
 import { useSearchParams } from 'next/navigation'
+import { useAppDispatch, useAppSelector } from '@/state/redux'
+
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+import { cleanParams } from '@/lib/utils'
+
+import FiltersFull from './FiltersFull'
 import FiltersBar from './FiltersBar'
 
 const SearchPage = () => {
     const searchParams = useSearchParams()
     const dispatch = useAppDispatch()
     const isFiltersFullOpen = useAppSelector((state) => state.global.isFiltersFullOpen)
+
+    useEffect(() => {
+        const initialFilters = Array.from(searchParams.entries()).reduce((acc: any, [key, value]) => {
+            if (key === 'priceRange' || key === 'squareFeet') {
+                acc[key] = value.split(',').map((v) => (v === '' ? null : Number(v)))
+            } else if (key === 'coordinates') {
+                acc[key] = value.split(',').map(Number)
+            } else {
+                acc[key] = value === 'any' ? null : value
+            }
+
+            return acc
+        }, {})
+
+        const cleanedFilters = cleanParams(initialFilters)
+        dispatch(setFilters(cleanedFilters))
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <div
@@ -24,7 +47,7 @@ const SearchPage = () => {
                         isFiltersFullOpen ? 'w-3/12 opacity-100 visible' : 'w-0 opacity-0 invisible'
                     }`}
                 >
-                    {/* <FiltersFull /> */}
+                    <FiltersFull />
                 </div>
                 {/* <Map /> */}
                 <div className='basis-4/12 overflow-y-auto'>{/* <Listings /> */}</div>
