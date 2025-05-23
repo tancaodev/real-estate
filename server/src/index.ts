3e5e7885fd12e411f23b9143165c65615485c592
Tan Cao <tancaodev.github@gmail.com>
2025-05-23 12:32:20 +0700

    update: cleanup changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use('/managers', authMiddleware(['manager']), managerRoutes)
 app.use('/leases', leaseRoutes)
 
 /* SERVER */
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3001
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`)
 })
