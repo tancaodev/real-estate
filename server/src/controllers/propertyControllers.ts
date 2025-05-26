4dd0fdae17f093cc14052a0507fd3dc777ffd885
Tan Cao <tancaodev.github@gmail.com>
2025-05-26 15:36:43 +0700

    feat: add s3 bucket

--- server/src/controllers/propertyControllers.ts
+++ server/src/controllers/propertyControllers.ts
@@ -159,23 +159,23 @@ export const createProperty = async (req: Request, res: Response): Promise<void>
         const files = req.files as Express.Multer.File[]
         const { address, city, state, country, postalCode, managerCognitoId, ...propertyData } = req.body
 
-        // const photoUrls = await Promise.all(
-        //     files.map(async (file) => {
-        //         const uploadParams = {
-        //             Bucket: process.env.S3_POCKET_NAME!,
-        //             Key: `properties/${Date.now()}-${file.originalname}`,
-        //             Body: file.buffer,
-        //             ContentType: file.mimetype
-        //         }
+        const photoUrls = await Promise.all(
+            files.map(async (file) => {
+                const uploadParams = {
+                    Bucket: process.env.S3_BUCKET_NAME!,
+                    Key: `properties/${Date.now()}-${file.originalname}`,
+                    Body: file.buffer,
+                    ContentType: file.mimetype
+                }
 
-        //         const uploadResult = await new Upload({
-        //             client: s3Client,
-        //             params: uploadParams
-        //         }).done()
+                const uploadResult = await new Upload({
+                    client: s3Client,
+                    params: uploadParams
+                }).done()
 
-        //         return uploadResult.Location
-        //     })
-        // )
+                return uploadResult.Location
+            })
+        )
 
         const geocodingUrl = `https://nominatim.openstreetmap.org/search?${new URLSearchParams({
             street: address,
@@ -205,7 +205,7 @@ export const createProperty = async (req: Request, res: Response): Promise<void>
         const newProperty = await prisma.property.create({
             data: {
                 ...propertyData,
-                // photoUrls,
+                photoUrls,
                 locationId: location.id,
                 managerCognitoId,
                 amenities: typeof propertyData.amenities === 'string' ? propertyData.amenities.split(',') : [],
